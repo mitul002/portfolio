81e696f8c820ce8a56d5171d80e62a85522ba1fa
mitul002 <mitul@example.com>
2026-03-25 23:26:34 +0600

    v1.2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  // On Vercel or local, host at root.
+  // Only for GitHub Pages, use /portfolio/
+  base: process.env.GITHUB_ACTIONS ? "/portfolio/" : "/",
 });
