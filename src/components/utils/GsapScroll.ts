8c00b88ac7ecd17186d0b7ff45c7dfc3acbb5a3e
mitul002 <mitul@example.com>
2026-03-26 01:20:53 +0600

    v1.22

--- src/components/utils/GsapScroll.ts
+++ src/components/utils/GsapScroll.ts
@@ -66,7 +66,12 @@ export function setCharTimeline(
       tl1
         .fromTo(character.rotation, { y: 0 }, { y: 0.7, duration: 1 }, 0)
         .to(camera.position, { z: 22 }, 0)
-        .fromTo(".character-model", { x: 0 }, { x: "-25%", duration: 1 }, 0)
+        .fromTo(
+          ".character-model",
+          { xPercent: -50, x: 0 },
+          { xPercent: -50, x: "-15vw", duration: 1 },
+          0
+        )
         .to(".landing-container", { opacity: 0, duration: 0.4 }, 0)
         .to(".landing-container", { y: "40%", duration: 0.8 }, 0)
         .fromTo(".about-me", { y: "-50%" }, { y: "0%" }, 0);
@@ -81,8 +86,14 @@ export function setCharTimeline(
         .to(".about-section", { opacity: 0, delay: 3, duration: 2 }, 0)
         .fromTo(
           ".character-model",
-          { pointerEvents: "inherit" },
-          { pointerEvents: "none", x: "-12%", delay: 2, duration: 5 },
+          { pointerEvents: "inherit", xPercent: -50 },
+          {
+            pointerEvents: "none",
+            xPercent: -50,
+            x: "-10vw",
+            delay: 2,
+            duration: 5,
+          },
           0
         )
         .to(character.rotation, { y: 0.92, x: 0.12, delay: 3, duration: 3 }, 0)
@@ -111,8 +122,8 @@ export function setCharTimeline(
       tl3
         .fromTo(
           ".character-model",
-          { y: "0%" },
-          { y: "-100%", duration: 4, ease: "none", delay: 1 },
+          { y: "0%", x: "-10vw", xPercent: -50 },
+          { y: "-100%", x: "-10vw", xPercent: -50, duration: 4, ease: "none", delay: 1 },
           0
         )
         .fromTo(".whatIDO", { y: 0 }, { y: "15%", duration: 2 }, 0)
